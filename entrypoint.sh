a689887d7a851a1de3ba755c8fb5a00b9bf3256b
nakajiss <68098853+nakajiss@users.noreply.github.com>
2022-04-21 11:48:19 +0700

    Execute all scripts in one pytest session

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 pip install --upgrade pip
-pip install --no-cache allure-pytest==2.9.45
-pip install --no-cache pytest==7.0.1
-pip install --no-cache selenium==4.1.2
+pip install --no-cache pytest
+pip install --no-cache allure-pytest
+pip install --no-cache selenium
 echo "Execution is being started"
 echo "**************************"
-for f in $@*.py; do
-  pytest "$f" --alluredir=results
-done
+
+pytest $@ --alluredir=results
+
 echo "**************************"
 echo "Execution has been completed, please check the artifacts to download the results."
