6688d96cfcca2a539eb6ae4b2e3da540bafc1006
Semyon Lugancev <68098853+nakajiss@users.noreply.github.com>
2023-09-13 11:08:28 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 pip install --upgrade pip
 pip install --no-cache pytest
-pip install --no-cache allure-pytest
-pip install --no-cache selenium
+pip install --no-cache termcolor
+pip install --no-cache selenium==4.9
+pip install --no-cache pytest-selenium
+pip install --no-cache allure-python-commons
 echo "Execution is being started"
 echo "**************************"
 
-if pytest $@ --alluredir=results | grep -q 'failed'; then
+if python3 -m pytest -v -s --driver Chrome --driver-path /bin/chromedriver $@ | grep -q 'failed'; then
   exit 1
 fi
 
